dae7bb9871276c4b9f81d487e794aabd1aac49d9
Rodrigo Carballo <rodrigo.puravida@protonmail.com>
2021-03-18 20:42:41 -0700

    Added text formatting to text labels

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.bottomTextField.delegate = self
+        self.topTextField.delegate = self
+        
+        //setting text atttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.defaultTextAttributes = memeTextAttributes
+        
     }
-
+    
+//Meme attributes
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -2.0]
+    
+//outlets
+    
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
 
 }
 

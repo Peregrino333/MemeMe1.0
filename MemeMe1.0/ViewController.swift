59d339b3a2048e534fc7c60ac23ddc32af51f2d1
Rodrigo Carballo <rodrigo.puravida@protonmail.com>
2021-03-22 19:26:24 -0700

    Added camera method and privacy

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,UITextFieldDelegate {
+    
+    
+//variables
+    var activeTextField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         bottomTextField.defaultTextAttributes = memeTextAttributes
         topTextField.defaultTextAttributes = memeTextAttributes
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeyboardNotifications()
     }
     
 //Meme attributes
@@ -44,11 +52,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
-        imagePicker.mediaTypes = ["public.image", "public.movie"]
-                
+        imagePicker.mediaTypes = ["public.image", "public.movie"]                
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
+    
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     
     func imagePickerController(_ pickerController: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
                     
@@ -65,6 +81,64 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         {
             dismiss(animated: true, completion: nil)
         }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+            textField.text = ""
+            
+            print("I am on text field")
+            
+            // we don't need to move the view up
+            self.activeTextField = textField
+            
+        }
+        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            
+            return true;
+        }
+
+    
+//keyboard notification
+    
+    func subscribeToKeyboardNotifications() {
+
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        }
+
+    func unsubscribeFromKeyboardNotifications() {
+
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+            
+            if (self.activeTextField == topTextField) {
+                view.frame.origin.y -= 0
+            }
+            else {
+                view.frame.origin.y -= getKeyboardHeight(notification)
+            }
+    }
+        
+    @objc func keyboardWillHide(_ notification:Notification) {
+
+              view.frame.origin.y = 0
+        }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+
+            let userInfo = notification.userInfo
+            let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+            return keyboardSize.cgRectValue.height
+        }
+
+    
     
 }
 

49a98c74c41b132d8b12f86a9edb6ccda95c230e
Rodrigo Carballo <rodrigo.puravida@protonmail.com>
2021-06-01 11:34:31 -0700

    Setting to Aspect Fit

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -16,6 +16,7 @@ struct Meme {
     var memedImage : UIImage
     }
     
+    
 //variables
     var activeTextField = UITextField()
 
@@ -49,17 +50,14 @@ struct Meme {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         super.viewWillAppear(animated)
         subscribeToKeyboardNotifications()
-        
     }
-    
-//Meme attributes
 
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth:  -2.0,
-                                               ]
+        NSAttributedString.Key.strokeWidth:  -2.0]
+                                               
     
 //outlets
     
